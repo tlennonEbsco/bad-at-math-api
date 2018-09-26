d932962eac485ef3a8f594584da3364361222e1b
Trent Lennon <tlennon@EBSCO.COM>
2018-09-26 12:00:48 -0400

    Replaced reload and supervisor with nodemon.

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3001
-const reload = require('reload')
 const fileUpload = require('express-fileupload')
 const fs = require('fs');
-
-var fileNames = [];
+const path = require('path');
+let morgan = require('morgan');
 
 app.use(fileUpload())
+app.use(morgan(':method :url :status ~:res[content-length] bytes - :response-time ms'));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
-app.set('port', process.env.PORT || 3001)
 
 app.get('/', (req, res) => res.send({message: 'Hello World!'}));
 
@@ -26,6 +24,7 @@ app.post('/upload', (req, res) => {
     //In Express 4, req.files is no longer available on the req object by default. To access uploaded files on the req.files object, use multipart-handling middleware like busboy, multer, formidable, multiparty, connect-multiparty, or pez.
     
     let sampleFile = req.files.sampleFile;
+
     sampleFile.mv('./public/' + sampleFile.name, err => {
         if(err)
             return res.status(500).send(err);
@@ -38,17 +37,23 @@ app.get('/file/:name', function(req, res) {
     res.download('./public/' + req.params.name);
 });
 
+let extFilter = ['.gitignore', ''];
+
+function filterByExtension(element) {
+    var extName = path.extname(element);
+    // console.log(element);
+    // console.log(path.extname(element));
+    return extFilter.indexOf(extName) == -1 ? true : false;
+}
+
 app.get('/file', function(req, res) {
-    if(fileNames.length === 0) {
-        var files = fs.readdirSync('./public/');
-        fileNames = files;
-    }
+    var files = fs.readdirSync('./public/');
+    const result = files.filter(file => filterByExtension(file));
+    fileNames = result;
         
     res.send({
         files: fileNames
     });
 });
 
-reload(app);
-
-app.listen(port, () => console.log('Example app listening on port ' + port + '!'))
+app.listen(port, () => console.log('"Bad At Math API" listening on port ' + port + '!'))
